7f6ec3853efdcb551f0bf621c827feb2a3be16da
hasithag <hasitha.gamage@sysco.com>
2020-03-21 22:45:12 +0530

    working test project

--- src/test/java/com/lazy/example1/TestSuiteExecution1.java
+++ src/test/java/com/lazy/example1/TestSuiteExecution1.java
@@ -2,9 +2,12 @@ package com.lazy.example1;
 
 import com.smile.lazy.LazyApplication;
 import com.smile.lazy.beans.LazySuite;
-import com.smile.lazy.beans.result.AssertionResultList;
+import com.smile.lazy.beans.executor.LazyExecutionData;
 import com.smile.lazy.manager.LazyManager;
+import com.smile.lazy.utils.JsonUtil;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.testng.Assert;
@@ -13,17 +16,20 @@ import org.testng.Assert;
 @SpringBootTest(classes = LazyApplication.class)
 public class TestSuiteExecution1 {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestSuiteExecution1.class);
+
     @Autowired
     private LazyManager lazyManager;
 
     @Test
-    public void testEmployee() {
+    public void testWithManager() {
         try {
             LazySuite sampleLazySuite = SampleLazySuite.populateSampleTestSuite();
-            AssertionResultList results = lazyManager.executeLazySuite(sampleLazySuite);
+            LazyExecutionData results = lazyManager.executeLazySuite(sampleLazySuite);
             Assert.assertNotNull(results);
-            Assert.assertNotNull(results.getResults());
-            results.getResults().forEach(result -> Assert.assertTrue(result.getPass(), result.getActualValue()));
+            Assert.assertNotNull(results.getTestSuiteExecutionData());
+//            String resultString = JsonUtil.getJsonStringFromObjectProtectedAndPublic(results);
+//            LOGGER.info("Execution results \n [{}]", resultString);
         } catch (Exception ex) {
             Assert.fail("Success scenarios should not be failed", ex);
         }
