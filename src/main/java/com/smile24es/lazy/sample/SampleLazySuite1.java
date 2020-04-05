f30c5e6e3419a663b455d4a65793c710c3e01769
hasithag <hasitha.gamage@sysco.com>
2020-04-05 23:32:17 +0530

    fix with maven

--- src/main/java/com/smile24es/lazy/sample/SampleLazySuite1.java
+++ src/main/java/com/smile24es/lazy/sample/SampleLazySuite1.java
@@ -1,12 +1,8 @@
 package com.smile24es.lazy.sample;
 
 import com.smile24es.lazy.sample.suites.AccountApiTestSuite;
-import com.smile24es.lazy.beans.DefaultValues;
 import com.smile24es.lazy.beans.LazySuite;
-import com.smile24es.lazy.beans.suite.Stack;
 import com.smile24es.lazy.exception.LazyCoreException;
-
-import static com.smile24es.lazy.utils.SampleDefaultValues.createDefaultValues;
 
 public class SampleLazySuite1 {
 
@@ -15,10 +11,7 @@ public class SampleLazySuite1 {
     }
 
     public static LazySuite populateSampleLazySuite() throws LazyCoreException {
-        DefaultValues defaultValues = createDefaultValues();
-        defaultValues.setContextPath("account-api");
-        Stack lazySuiteStack = new Stack(defaultValues);
-        LazySuite lazySuite = new LazySuite("Sample lazy suite 1", lazySuiteStack);
+        LazySuite lazySuite = new LazySuite("Sample lazy suite 1");
         lazySuite.getTestSuites().add(AccountApiTestSuite.getAccountApiTestSuite());
         return lazySuite;
     }
--- src/main/java/com/smile24es/lazy/sample/suites/AccountApiTestSuite.java
+++ src/main/java/com/smile24es/lazy/sample/suites/AccountApiTestSuite.java
@@ -16,6 +16,7 @@ public class AccountApiTestSuite {
 
     public static TestSuite getAccountApiTestSuite() throws LazyCoreException {
         TestSuite testSuite1 = new TestSuite("Account Test Suite");
+        testSuite1.getStack().getDefaultValues().setContextPath("account-api");
         testSuite1.getTestScenarios().add(getAccountCreationTestScenario());
         testSuite1.setAssignGroups(Arrays.asList("account-bvt"));
         return testSuite1;
