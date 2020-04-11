217b1e87f7d80a64c7399697f02ce1a316264c0d
hasithag <hasitha.gamage@sysco.com>
2020-04-12 01:36:14 +0530

    sample tests

--- src/main/java/com/smile24es/lazy/sample0/student/StudentApiTestCases.java
+++ src/main/java/com/smile24es/lazy/sample0/student/StudentApiTestCases.java
@@ -2,11 +2,6 @@ package com.smile24es.lazy.sample0.student;
 
 import com.smile24es.lazy.beans.suite.TestCase;
 import com.smile24es.lazy.exception.LazyCoreException;
-import com.smile24es.lazy.sample0.dto.StudentDto;
-import com.smile24es.lazy.sample0.dto.StudentSetting;
-import com.smile24es.lazy.sample1.account.AccountApiCall;
-import com.smile24es.lazy.sample1.dto.AccountSetting;
-import com.smile24es.lazy.sample1.dto.ErrorTo;
 
 import static com.smile24es.lazy.sample0.student.StudentApiCall.createStudentApiCall;
 import static com.smile24es.lazy.sample0.student.StudentApiCall.getStudentById;
